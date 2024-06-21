49aef2b30b397325949daa6c46efada13cd226fd
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-06-21 23:25:10 +0000

    Finish Delete Routes

--- src/api/routes.py
+++ src/api/routes.py
@@ -149,59 +149,43 @@ def handle_customer_signup():
     customer = Customer.query.filter_by(email=email).one_or_none()
     if customer:
         return jsonify({"msg": "An account associated with the email already exists"}), 409
-    customer = Customer(first_name=first_name, last_name=last_name, phone=phone, email=email)
+    customer = Customer(first_name=first_name, last_name=last_name, phone=phone, email=email, password=password)
     db.session.add(customer)
     db.session.commit()
     db.session.refresh(customer)
     response_body = {"msg": "Account succesfully created!", "customer":customer.serialize()}
     return jsonify(response_body), 201
 
-@api.route('/customer/login', methods=['POST'])
-def handle_customer_login():
-    email = request.json.get("email", None)
-    password = request.json.get("password", None)
-    if email is None or password is None:
-        return jsonify({"msg": "No email or password"}), 400
-    customer = Customer.query.filter_by(email=email).one_or_none()
-    if customer is None:
-        return jsonify({"msg": "No such user"}), 404
-    if customer.password != password:
-        return jsonify({"msg": "Bad email or password"}), 401
-
-    access_token = create_access_token(identity=customer.id, additional_claims={"role": "customer"})
-
-    return jsonify(access_token=access_token, customer_id=customer.id), 201
-
-@api.route('/customer/edit-by-customer', methods=['PUT'])
-@jwt_required()
-def handle_customer_edit_by_customer():
-    email = request.json.get("email")
-    first_name = request.json.get("first_name")
-    last_name = request.json.get("last_name")
-    address = request.json.get("address")
-    phone = request.json.get("phone")
+# @api.route('/customer/edit-by-customer', methods=['PUT'])
+# @jwt_required()
+# def handle_customer_edit_by_customer():
+#     email = request.json.get("email")
+#     first_name = request.json.get("first_name")
+#     last_name = request.json.get("last_name")
+#     address = request.json.get("address")
+#     phone = request.json.get("phone")
     
-    if email is None or first_name is None or last_name is None or address is None or phone is None:
-        return jsonify({"msg": "Some fields are missing in your request"}), 400
+#     if email is None or first_name is None or last_name is None or address is None or phone is None:
+#         return jsonify({"msg": "Some fields are missing in your request"}), 400
    
-    customer = Customer.query.filter_by(id=get_jwt_identity()).first()
-    if customer is None:
-        return jsonify({"msg": "No customer found"}), 404
+#     customer = Customer.query.filter_by(id=get_jwt_identity()).first()
+#     if customer is None:
+#         return jsonify({"msg": "No customer found"}), 404
     
-    customer.email=email 
-    customer.first_name=first_name   
-    customer.last_name=last_name 
-    customer.address=address    
-    customer.phone=phone
-    db.session.commit()
-    db.session.refresh(customer)
+#     customer.email=email 
+#     customer.first_name=first_name   
+#     customer.last_name=last_name 
+#     customer.address=address    
+#     customer.phone=phone
+#     db.session.commit()
+#     db.session.refresh(customer)
     
-    response_body = {"msg": "Account succesfully edited!", "customer":customer.serialize()}
-    return jsonify(response_body), 201
+#     response_body = {"msg": "Account succesfully edited!", "customer":customer.serialize()}
+#     return jsonify(response_body), 201
 
-@api.route('/user/get-customer/<int:cust_id>', methods=['GET'])
+@api.route('/customer/<int:cust_id>', methods=['GET'])
 # @mentor_required()
-def get_customer(cust_id):
+def customer_by_id(cust_id):
     # current_user_id = get_jwt_identity()
     # current_user = User.query.get(current_user_id)
 
@@ -227,3 +211,19 @@ def get_all_customers():
     customers = Customer.query.all()
     return jsonify([customer.serialize() for customer in customers]), 200
 
+@api.route('/customer/delete/<int:cust_id>', methods =['DELETE'])
+def delete_customer(cust_id):
+    customer = Customer.query.get(cust_id)
+    if customer is None:
+        return jsonify({"msg": "work order not found" }), 404
+    
+    # picture_public_ids = [image.image_url.split("/")[-1].split(".")[0] for image in work_order.images]
+
+    # for public_id in picture_public_ids:
+    #     delete_response = destroy(public_id)
+    #     if delete_response.get("result") != "ok":
+    #         print(f"Failed to delete picture with public ID: {public_id}")
+
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({"msg": "customer successfully deleted"}), 200
