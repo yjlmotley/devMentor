9f202f15c58bdf39712c166a4a64b5a12f402eed
yjlmotley <yjlmotley@gmail.com>
2024-09-09 23:15:03 -0700

    navbar & backend styling

--- src/api/admin.py
+++ src/api/admin.py
@@ -6,8 +6,10 @@ from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    # app.config['FLASK_ADMIN_SWATCH'] = 'journal'
+    app.config['FLASK_ADMIN_SWATCH'] = 'sandstone'
+    # app.config['FLASK_ADMIN_SWATCH'] = 'cyborg'
+    admin = Admin(app, name='devMentor Admin', template_mode='bootstrap3')
 
     
     admin.add_view(ModelView(Customer, db.session))
