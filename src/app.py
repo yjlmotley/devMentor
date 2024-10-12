0a057d667d4f2a11ae2bcf92dcd695f092dce3fd
Michael Mirisciotta <151809512+Michaelmira@users.noreply.github.com>
2024-10-12 23:57:09 +0000

    SAVE FOR DEPLOY

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, request, jsonify, url_for, send_from_directory
 from flask_migrate import Migrate
 from flask_swagger import swagger
+from flask_cors import CORS
 from api.utils import APIException, generate_sitemap
 from api.models import db
 from api.routes import api
@@ -12,76 +13,61 @@ from api.admin import setup_admin
 from api.commands import setup_commands
 from flask_jwt_extended import JWTManager
 
-# #######################################################################
+import cloudinary
 import cloudinary.uploader as uploader
-# #######################################################################
 
-# from models import Person
-
-ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../public/')
+ENV = os.getenv("FLASK_ENV", "development")
+static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
-app.config["JWT_SECRET_KEY"] = ""
 app.url_map.strict_slashes = False
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 7*24*60*60*52
-JWTManager(app)
 
-# Cloudinary 
-
-# UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-app.config['UPLOAD_FOLDER'] = os.environ.get('UPLOAD_FOLDER')
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config.from_mapping(
-    CLOUDINARY_URL=os.environ.get("CLOUDINARY_URL")
-)
-          
-# cloudinary.config( 
-#   cloud_name = "dufs8hbca", 
-#   api_key = "442387455219856", 
-#   api_secret = "shYvKGsmGcMnNGftA-RK7Hy7eww" 
-# )
-
-# database condiguration
+# Database configuration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
-        "postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+
+app.config["JWT_SECRET_KEY"] = os.environ.get("FLASK_APP_KEY", "sample key")
+app.config["JWT_ALGORITHM"] = "HS256"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 24 * 60 * 60
+jwt = JWTManager(app)
+
+app.config['UPLOAD_FOLDER'] = os.environ.get('UPLOAD_FOLDER')
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['CLOUDINARY_URL'] = os.environ.get("CLOUDINARY_URL")
+
+UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
 
-# add the admin
+# Allow CORS requests to this API
+CORS(app)
+
+# Add the admin
 setup_admin(app)
 
-# add the admin
+# Add the admin
 setup_commands(app)
 
-# Add all endpoints form the API with a "api" prefix
+# Add all endpoints from the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
 
 # Handle/serialize errors like a JSON object
-
-
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-
-
+# Generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
 
-# any other endpoint will try to serve it like a static file
-
-
+# Any other endpoint will try to serve it like a static file
 @app.route('/<path:path>', methods=['GET'])
 def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
@@ -90,8 +76,23 @@ def serve_any_other_file(path):
     response.cache_control.max_age = 0  # avoid cache memory
     return response
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Retrieve form data
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    email = request.form['email']
+    phone_number = request.form['phone_number']
+    vin_number = request.form['vin_number']
+    license_plate = request.form['license_plate']
+    text_area = request.form['text_area']
 
-# this only runs if `$ python src/main.py` is executed
+    # Process the form data here
+
+    # Return a response (confirmation message)
+    return jsonify({'message': 'Form submitted successfully'})
+
+# This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=False)
